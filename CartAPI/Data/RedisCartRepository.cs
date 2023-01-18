fdca067cd8afe9af951c04c37a820c797797d424
elsieliuliu <elsieliu.uw@gmail.com>
2023-01-17 18:55:35 -0800

    Add CartAPI and integrate with WebMVC

---
+++ CartAPI/Data/RedisCartRepository.cs
@@ -0,0 +1,38 @@
+﻿using CartAPI.Models;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.Text.Json.Serialization;
+
+namespace CartAPI.Data
+{
+    public class RedisCartRepository : ICartRepository
+    {
+        private readonly ConnectionMultiplexer _redis;
+        private readonly IDatabase _database;
+        public RedisCartRepository(ConnectionMultiplexer redis) 
+        { 
+            _redis = redis;
+            _database = redis.GetDatabase();
+        }
+        public Task<bool> DeleteCartAsync(string cartId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Cart> GetCartAsync(string cartId)
+        {
+           var data = await _database.StringGetAsync(cartId); 
+           if (data.IsNullOrEmpty)
+            {
+                return null; 
+            }
+           JsonConvert
+        }
+
+        public Task<Cart> UpdateCartAsync(Cart basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
